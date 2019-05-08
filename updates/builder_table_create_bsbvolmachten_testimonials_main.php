10482c06af05e046ec6af2d5f71092cf1ce58f09
Frenk Smeets <Dah@live.nl>
2019-05-08 20:41:35 +0200

    Add files via upload

---
+++ updates/builder_table_create_bsbvolmachten_testimonials_main.php
@@ -0,0 +1,24 @@
+<?php namespace BsbVolmachten\Testimonials\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateBsbvolmachtenTestimonialsMain extends Migration
+{
+    public function up()
+    {
+        Schema::create('bsbvolmachten_testimonials_main', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('name', 255);
+            $table->string('description', 255);
+            $table->string('review', 400);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('bsbvolmachten_testimonials_main');
+    }
+}
---
+++ updates/builder_table_update_bsbvolmachten_testimonials_main.php
@@ -0,0 +1,23 @@
+<?php namespace BsbVolmachten\Testimonials\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateBsbvolmachtenTestimonialsMain extends Migration
+{
+    public function up()
+    {
+        Schema::table('bsbvolmachten_testimonials_main', function($table)
+        {
+            $table->integer('sort_order');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('bsbvolmachten_testimonials_main', function($table)
+        {
+            $table->dropColumn('sort_order');
+        });
+    }
+}
